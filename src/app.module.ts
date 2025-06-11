087ec71ff471561fcc27eec7a179ad945bf7a7f5
MJ <mahanandhanait@gmail.com>
2025-06-11 12:53:09 +0530

    workable image_file_uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProtectedController } from './protected/protected.controller';
-
+import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-auth-db'),
     AuthModule,
     UsersModule,
+    ProductsModule,
   ],
   controllers: [ProtectedController],
 })
---
+++ src/products/products.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { extname } from 'path';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const uniqueName = `${uuidv4()}${extname(file.originalname)}`;
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  async create(
+    @Body() dto: CreateProductDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const imageUrl = file ? `/uploads/${file.filename}` : '';
+    return this.productsService.create(dto, imageUrl);
+  }
+
+  @Get()
+  async findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
+    return this.productsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.productsService.remove(id);
+  }
+}
